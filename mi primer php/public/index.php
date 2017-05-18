d04fff1613421e71807ef0c9cb7f15a07a7b3e21
esolorzanob <esolorzano@renovatiocloud.com>
2017-05-18 10:57:53 -0600

    primera clase

---
+++ mi primer php/public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h1>My first PHP page</h1>
+
+
+<?php
+$nombre = "Esteban";
+echo "<h1 class='nombre'> Bienvenido $nombre </h1>";
+echo "Hello World!";
+?>
+
+</body>
+</html>
